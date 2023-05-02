a7d75a55da8e90869c65b84775f4115c53bab834
Isabelle Queiroz <113696416+isabelleqga@users.noreply.github.com>
2023-05-02 18:33:39 -0300

    Add files via upload

---
+++ mysqlphp/ptdomestico master/bot.php
@@ -0,0 +1,47 @@
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal-exclusao">
+    <h4 id="tituloAtencao">
+        <span class="material-icons">
+            report_problem
+        </span>
+        Atenção!
+    </h4>
+    <h3 id="textoModal"></h3>
+    <div class="botoes-confirmacao">
+        <button class="btn btn-secondary" onclick="fecharModal()">Cancelar</button>
+        <button class="btn btn-danger" onclick="fecharModal()">Excluir</button>
+    </div>
+</div>
+<div class="mask" onclick="fecharModal()"></div>
+</body>
+
+<!--   Core JS Files   -->
+<script src="/ptdomestico master/assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="/ptdomestico master/assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Checkbox, Radio & Switch Plugins -->
+<script src="/ptdomestico master/assets/js/bootstrap-checkbox-radio.js"></script>
+
+<!--  Charts Plugin -->
+<script src="/ptdomestico master/assets/js/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="/ptdomestico master/assets/js/bootstrap-notify.js"></script>
+
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+<!-- Paper Dashboard Core javascript and methods for Demo purpose -->
+<script src="/ptdomestico master/assets/js/paper-dashboard.js"></script>
+
+<!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+<script src="/ptdomestico master/assets/js/demo.js"></script>
+
+
+<script src="<?php echo $diretorio; ?>assets/js/scriptModal.js"></script>
+
+
+</html>
